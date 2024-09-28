291786b8a719838b1631dbc0cb8af84c92676d25
BarneyStinson73 <1905040@ugrad.cse.buet.ac.bd>
2024-09-28 15:51:31 +0600

    not letting into the website for ddos try ,i assume ;(

--- PlayWright/start.py
+++ PlayWright/start.py
@@ -7,11 +7,21 @@ def run(playwright: Playwright):
     page = browser.new_page()
     page.goto(st_url)
     for link in page.locator('a[data-selenium="miniProductPageDetailsGridViewNameLink" ]').all():
-        # print(link.text_content())
-        p=browser.new_page(base_url="https://www.bhphotovideo.com")
+        # opening each link in a new tab
+        p=browser.new_page(base_url="https://www.bhphotovideo.com/")
         url= link.get_attribute('href')
         if url is not None:
             p.goto(url)
-            p.close()
+            try:
+                # Wait for the script tag to be available before extracting its content
+                p.wait_for_selector("script[type='application/ld+json']", timeout=5000)
+                product_info = p.locator("script[type='application/ld+json']").text_content()
+                print(product_info)  # this will print the product info in json format
+            except TimeoutError:
+                print(f"Could not find the product info script tag for {url}")
+            finally:
+                p.close()
+         
+        
 with sync_playwright() as p:
     run(p)
