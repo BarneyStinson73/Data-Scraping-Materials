36c1ef73b5d3044f980b163c35e547d1d6679391
BarneyStinson73 <1905040@ugrad.cse.buet.ac.bd>
2024-09-28 15:14:00 +0600

    DDOS o mara jaito xD

--- PlayWright/start.py
+++ PlayWright/start.py
@@ -1,8 +1,17 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Playwright, TimeoutError
 
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False,slow_mo=100)
+def run(playwright: Playwright):
+    st_url = "https://www.bhphotovideo.com/deals-promotions-coupons/"
+    chrome = playwright.chromium
+    browser = chrome.launch(headless=False)
     page = browser.new_page()
-    page.goto("https://github.com")
-    browser.close()
+    page.goto(st_url)
+    for link in page.locator('a[data-selenium="miniProductPageDetailsGridViewNameLink" ]').all():
+        # print(link.text_content())
+        p=browser.new_page(base_url="https://www.bhphotovideo.com")
+        url= link.get_attribute('href')
+        if url is not None:
+            p.goto(url)
+            p.close()
+with sync_playwright() as p:
+    run(p)
